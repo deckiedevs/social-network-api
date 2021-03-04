b951e394a5e2d5dc0ff268080e219cc2996e82e4
Mila Decker <milacdecker@gmail.com>
2021-03-03 20:19:01 -0500

    added user and thought routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,3 +1,24 @@
 const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    addThought,
+    updateThought,
+    deleteThought
+} = require('../../controllers/thought-controller');
+
+// set up GET all thoughts at /api/thoughts
+router.route('/')
+    .get(getAllThoughts)
+
+// set up POST at /api/thoughts/:userId
+router.route('/:userId')
+    .post(addThought)
+
+// set up GET single, PUT, and DELETE thought at /api/thoughts/:userId/:thoughtId
+router.route('/:userId/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought)
 
 module.exports = router;
